d0e2894c1483d61fed27ba2f781c1ae26711760a
B1boid <amigodos@yandex.ru>
2022-01-06 18:07:23 +0300

    window bug fixed

--- src/components/pages/main/GreetingText.jsx
+++ src/components/pages/main/GreetingText.jsx
@@ -2,15 +2,13 @@ import React from 'react';
 
 const GreetingText = () => {
     return (
-        <div className="w-1/2 space-y-14">
+        <div className="max-w-7xl mx-auto w-1/2 space-y-14 h-screen">
             <div
-                className="text-3xl text-left md:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-indigo-600 to-pink-500 flex-end">
-                Welcome to Nea Sea
+                className="text-3xl text-left md:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-mjol-blue-base to-green-200 flex-end">
+                Welcome to MjolNear
             </div>
             <div className="text-sm md:text-lg font-mono">
-                The first NFT marketplace that enables creators to choose and embed licenses when they mint NFTs.
-                <br/>
-                Creators know what they are selling, collectors know what they are buying. Join the revolution.
+                The first Open NFT marketplace on Near blockchain.
             </div>
         </div>
     );
