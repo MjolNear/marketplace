2789c4b7df0322a2f92d059d29276d78e3e781f6
Danielto1404 <daniilkorolev00@gmail.com>
2022-01-07 15:04:50 +0300

    Fix small nft link + preview image layout

--- src/components/nft-item/NftItem.jsx
+++ src/components/nft-item/NftItem.jsx
@@ -16,29 +16,31 @@ const NftItem = ({nft}) => {
         <NftBoxContainer>
             <Link to={previewLink}>
                 <NftImage path={nft.mediaURL}/>
-                <div className="px-5">
+            </Link>
+            <div className="px-5">
+                <Link to={previewLink}>
                     <NftTitle title={nft.title}/>
-                    <NftCollection collectionLogoLink="MOCK"
-                                   collectionLink="MOCK"
-                                   collectionName="MOCK"/>
-                    <div className="grid place-items-end">
-                        <div className="inline-flex space-x-1 text-tiny-2 xs:text-tiny-3 2xl:text-tiny-4 items-center">
-                            <NftMintedLink mintedName={nft.mintSite.name}
-                                           mintedLink={nft.mintSite.nftLink}/>
-                            {nft.mintSite.name === 'Non-verified contract' ? (
-                                <></>
-                            ) : (
-                                <NftVerifiedStatus/>
-                            )
-                            }
-                        </div>
-                    </div>
-                    <hr className="ring-1 ring-mjol-purple-dark border-none"/>
-                    <div className="py-2">
-                        <SmallNftPrice price={nft.price} isListed={isListed}/>
+                </Link>
+                <NftCollection collectionLogoLink="MOCK"
+                               collectionLink="MOCK"
+                               collectionName="MOCK"/>
+                <div className="grid place-items-end">
+                    <div className="inline-flex space-x-1 text-tiny-2 xs:text-tiny-3 2xl:text-tiny-4 items-center">
+                        <NftMintedLink mintedName={nft.mintSite.name}
+                                       mintedLink={nft.mintSite.nftLink}/>
+                        {nft.mintSite.name === 'Non-verified contract' ? (
+                            <></>
+                        ) : (
+                            <NftVerifiedStatus/>
+                        )
+                        }
                     </div>
                 </div>
-            </Link>
+                <hr className="ring-1 ring-mjol-purple-dark border-none"/>
+                <div className="py-2">
+                    <SmallNftPrice price={nft.price} isListed={isListed}/>
+                </div>
+            </div>
         </NftBoxContainer>
     );
 };
