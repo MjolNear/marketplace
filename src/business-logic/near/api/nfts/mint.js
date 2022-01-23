a8f4817ad9f0ed8f842f0c220a29cb0b65f94229
B1boid <amigodos@yandex.ru>
2022-01-23 23:35:46 +0300

    create collection & mint to collection

--- src/business-logic/near/api/nfts/mint.js
+++ src/business-logic/near/api/nfts/mint.js
@@ -2,21 +2,38 @@ import {GAS, SM_DEPOSIT} from "../../constants";
 import {getAccountId} from "../../enviroment/near";
 import {functionCall} from "../rpc";
 
-export function mintToCommonCollection(tokenMetadata, payout) {
+export function mintToCommonCollection(tokenMetadata, payout, collectionId) {
     const commonContactId = 'mjol.near';
-    const tokenId = 'token-' + new Date().getTime();
 
     const args = {
-        token_id: tokenId,
         token_owner_id: getAccountId(),
         token_metadata: tokenMetadata,
     };
     if (payout !== null) {
         args["payout"] = payout
     }
+    if (collectionId !== null) {
+        args["collection_id"] = collectionId
+    }
     return functionCall({
         contractId: commonContactId,
         methodName: 'nft_mint',
+        args,
+        gas: GAS,
+        attachedDeposit: SM_DEPOSIT
+    })
+}
+
+export function createCollection(collectionMetadata) {
+    const commonContactId = 'mjol.near';
+
+    const args = {
+        metadata: collectionMetadata,
+    };
+
+    return functionCall({
+        contractId: commonContactId,
+        methodName: 'create_collection',
         args,
         gas: GAS,
         attachedDeposit: SM_DEPOSIT
